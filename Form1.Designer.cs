c70751f209a685099074584f6303a6a6b5b934e9
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2022-03-01 18:17:45 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace RegistryCursorTutorial
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.picSelectedCursor = new System.Windows.Forms.PictureBox();
+			this.lblCursorPath = new System.Windows.Forms.Label();
+			this.btnLoadCursor = new System.Windows.Forms.Button();
+			this.btnHoverMe = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picSelectedCursor)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(45, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Cursor:";
+			// 
+			// picSelectedCursor
+			// 
+			this.picSelectedCursor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.picSelectedCursor.Location = new System.Drawing.Point(402, 38);
+			this.picSelectedCursor.Name = "picSelectedCursor";
+			this.picSelectedCursor.Size = new System.Drawing.Size(32, 32);
+			this.picSelectedCursor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.picSelectedCursor.TabIndex = 1;
+			this.picSelectedCursor.TabStop = false;
+			// 
+			// lblCursorPath
+			// 
+			this.lblCursorPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblCursorPath.Location = new System.Drawing.Point(63, 11);
+			this.lblCursorPath.Name = "lblCursorPath";
+			this.lblCursorPath.Size = new System.Drawing.Size(387, 15);
+			this.lblCursorPath.TabIndex = 2;
+			this.lblCursorPath.Text = "CURSOR_PATH";
+			this.lblCursorPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnLoadCursor
+			// 
+			this.btnLoadCursor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnLoadCursor.Location = new System.Drawing.Point(456, 3);
+			this.btnLoadCursor.Name = "btnLoadCursor";
+			this.btnLoadCursor.Size = new System.Drawing.Size(79, 31);
+			this.btnLoadCursor.TabIndex = 3;
+			this.btnLoadCursor.Text = "Load Cursor";
+			this.btnLoadCursor.UseVisualStyleBackColor = true;
+			this.btnLoadCursor.Click += new System.EventHandler(this.LoadCursor);
+			// 
+			// btnHoverMe
+			// 
+			this.btnHoverMe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnHoverMe.Location = new System.Drawing.Point(440, 38);
+			this.btnHoverMe.Name = "btnHoverMe";
+			this.btnHoverMe.Size = new System.Drawing.Size(95, 33);
+			this.btnHoverMe.TabIndex = 4;
+			this.btnHoverMe.Text = "Hover Me";
+			this.btnHoverMe.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(537, 75);
+			this.Controls.Add(this.btnHoverMe);
+			this.Controls.Add(this.btnLoadCursor);
+			this.Controls.Add(this.picSelectedCursor);
+			this.Controls.Add(this.lblCursorPath);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(854, 114);
+			this.MinimumSize = new System.Drawing.Size(409, 114);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Registry Cursor Tutorial";
+			((System.ComponentModel.ISupportInitialize)(this.picSelectedCursor)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox picSelectedCursor;
+		private System.Windows.Forms.Label lblCursorPath;
+		private System.Windows.Forms.Button btnLoadCursor;
+		private System.Windows.Forms.Button btnHoverMe;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace RegistryCursorTutorial
+{
+	public partial class Form1 : Form
+	{
+		// determines whether or not the program
+		// will use pure code to load the cursor
+		// or the referenced dll as a shortcut
+		private static bool dll = false;
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr LoadCursorFromFile(string CursorFile);
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			if (dll == true)
+			{
+				string cursorPath_Link = RegistryCursor.GetCursorPath(RegistryCursor.CursorType.Link_Select);
+				Cursor cursor_Link = RegistryCursor.LoadFromPath(cursorPath_Link);
+
+				btnLoadCursor.Cursor = cursor_Link;
+
+				string cursorPath_Normal = RegistryCursor.GetCursorPath(RegistryCursor.CursorType.Normal_Select);
+				lblCursorPath.Text = cursorPath_Normal;
+
+				Bitmap curBitmap = ToBitmap(RegistryCursor.LoadFromPath(cursorPath_Normal));
+				picSelectedCursor.Image = curBitmap;
+				btnHoverMe.Cursor = RegistryCursor.LoadFromPath(cursorPath_Normal);
+			}
+			else if (dll == false)
+			{
+				// open the Registry key containing the string values
+				// holding the file paths to the different cursors
+				// of the current cursor scheme
+				RegistryKey cursorRegKey = Registry.CurrentUser.OpenSubKey("Control Panel").OpenSubKey("Cursors");
+
+				// get the "Normal" cursor file path
+				// get the "Link" cursor file path
+				string regKey_NormalCursor = cursorRegKey.GetValue("Arrow").ToString();
+				string regKey_LinkCursor = cursorRegKey.GetValue("Hand").ToString();
+
+				// use the native Windows api to get the cursor handle from the given file path
+				IntPtr normalCursor_Handle = LoadCursorFromFile(regKey_NormalCursor);
+				IntPtr linkCursor_Handle = LoadCursorFromFile(regKey_LinkCursor);
+
+				// load the cursor from the cursor handle
+				Cursor normalCursor = new Cursor(normalCursor_Handle);
+				Cursor linkCursor = new Cursor(linkCursor_Handle);
+
+				// set the basic starting ui settings
+				btnLoadCursor.Cursor = linkCursor;
+				btnHoverMe.Cursor = normalCursor;
+				lblCursorPath.Text = regKey_NormalCursor;
+				Bitmap curNormal_Bitmap = ToBitmap(normalCursor);
+				picSelectedCursor.Image = curNormal_Bitmap;
+			}
+		}
+
+		private void LoadCursor(object sender, EventArgs e)
+		{
+			using (OpenFileDialog ofd = new OpenFileDialog
+			{
+				Title = "Please select a cursor to load...",
+				Filter = "Cursor (All)|*.cur; *.ani|" +
+						"Cursor (Static)|*.cur|" +
+						"Cursor (Animated)|*.ani",
+				FilterIndex = 1,
+			})
+			{
+				if (ofd.ShowDialog() == DialogResult.OK)
+				{
+					if (dll == true)
+					{
+						// use the RegistryCursor.LoadFromPath(string) method to get the cursor from the file path
+						string cursorPath = ofd.FileName;
+						Cursor cursor = RegistryCursor.LoadFromPath(cursorPath);
+
+						// set the "Hover Me" button's cursor to the loaded cursor
+						btnHoverMe.Cursor = cursor;
+
+						// display the new cursor in the picture box so the user
+						// can see without constantly hovering over the
+						// "Hover Me" button
+						Bitmap curBitmap = ToBitmap(cursor);
+						picSelectedCursor.Image = curBitmap;
+					}
+					else if (dll == false)
+					{
+						// use the native Windows api to get the cursor handle from the given file path
+						string cursorPath = ofd.FileName;
+						IntPtr cursorHandle = LoadCursorFromFile(cursorPath);
+
+						// create the cursor from the cursor handle
+						Cursor cursor = new Cursor(cursorHandle);
+
+						// display the file path and cursor image
+						Bitmap cursorBitmap = ToBitmap(cursor);
+						picSelectedCursor.Image = cursorBitmap;
+						lblCursorPath.Text = cursorPath;
+						btnHoverMe.Cursor = cursor;
+					}
+				}
+			}
+		}
+
+		// simple function to draw a cursor to a bitmap to display it visually
+		private Bitmap ToBitmap(Cursor Cursor)
+		{
+			Bitmap curBitmap = new Bitmap(Cursor.Size.Width, Cursor.Size.Height);
+
+			using (Graphics g = Graphics.FromImage(curBitmap))
+			{
+				Cursor.Draw(g, new Rectangle(0, 0, curBitmap.Width, curBitmap.Height));
+			}
+
+			return curBitmap;
+		}
+	}
+}
